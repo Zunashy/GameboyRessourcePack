6b0274b42ade4f22f032058b53d64c4dc53dbae4
Zunashy <guyguyshzunashy@gmail.com>
2020-03-06 03:29:06 +0100

    first

---
+++ data/scripts/game_manager.lua
@@ -0,0 +1,27 @@
+-- Script that creates a game ready to be played.
+
+-- Usage:
+-- local game_manager = require("scripts/game_manager")
+-- local game = game_manager:create("savegame_file_name")
+-- game:start()
+
+require("scripts/multi_events")
+local initial_game = require("scripts/initial_game")
+
+local game_manager = {}
+
+-- Creates a game ready to be played.
+function game_manager:create(file)
+
+  -- Create the game (but do not start it).
+  local exists = sol.game.exists(file)
+  local game = sol.game.load(file)
+  if not exists then
+    -- This is a new savegame file.
+    initial_game:initialize_new_savegame(game)
+  end
+
+  return game
+end
+
+return game_manager
