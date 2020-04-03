38fcb713cdc765297a82860f81b39a1782ab3f49
Zunashy <guyguyshzunashy@gmail.com>
2020-04-03 04:19:06 +0200

    update tileset contour

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -14,7 +14,7 @@ function initial_game:initialize_new_savegame(game)
 
   -- You can modify this function to set the initial life and equipment
   -- and the starting location.
-  game:set_starting_location("first_map", nil)  -- Starting location.
+  game:set_starting_location("lava", nil)  -- Starting location.
 
   game:set_max_life(12)
   game:set_life(game:get_max_life())
